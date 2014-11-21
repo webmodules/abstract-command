c7c2307de5b62eefbad0e1ed945d1ea531c03153
Nathan Rajlich <nathan@tootallnate.net>
2014-11-21 15:15:08 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+abstract-command
+============
+### Abstract `Command` base class to make implementing custom commands easier
+
+[![Sauce Test Status](https://saucelabs.com/browser-matrix/abstract-command.svg)](https://saucelabs.com/u/abstract-command)
+
+[![Build Status](https://travis-ci.org/webmodules/abstract-command.svg?branch=master)](https://travis-ci.org/webmodules/abstract-command)
+
+Just implement `_execute()`, `_queryEnabled()` and `_queryState()` override
+functions on your subclass of `AbstractCommand`!
+
+
+Installation
+------------
+
+``` bash
+$ npm install abstract-command
+```
---
+++ index.ts
@@ -0,0 +1,70 @@
+/// <reference path='types.d.ts' />
+
+/**
+ * TypeScript dependencies.
+ */
+
+import Command = require('command');
+import currentRange = require('current-range');
+import currentSelection = require('current-selection');
+import isBackward = require('selection-is-backward');
+import setRange = require('selection-set-range');
+
+/**
+ * Abstract `Command` base class to make implementing custom commands easier.
+ *
+ * @class
+ * @public
+ */
+
+class AbstractCommand implements Command {
+  document: Document;
+
+  constructor(doc: Document = document) {
+    this.document = doc;
+  }
+
+  execute(range?: Range, value?: any): void {
+    var hasRange: boolean = !!(range && range instanceof Range);
+    var backward: boolean;
+    var selection: Selection;
+
+    if (!hasRange) {
+      selection = currentSelection(this.document);
+      backward = isBackward(selection);
+      range = currentRange(selection);
+    }
+
+    this._execute(range, value);
+
+    if (!hasRange) {
+      // when no Range was explicitly passed in then we must reset
+      // the document's Selection
+      setRange(selection, range, backward);
+    }
+  }
+
+  queryEnabled(range?: Range): boolean {
+    if (!range) range = currentRange(this.document);
+    return Boolean(this._queryEnabled(range));
+  }
+
+  queryState(range?: Range): boolean {
+    if (!range) range = currentRange(this.document);
+    return Boolean(this._queryState(range));
+  }
+
+  protected _execute(range: Range, value?: any): void {
+    throw new Error('implement _execute()!');
+  }
+
+  protected _queryEnabled(range: Range): void {
+    throw new Error('implement _queryEnabled()!');
+  }
+
+  protected _queryState(range: Range): void {
+    throw new Error('implement _queryState()!');
+  }
+}
+
+export = AbstractCommand;
